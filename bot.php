1772ead27a148f682b240558bee09266b020e073
alexander171294 <alexander171294@gmail.com>
2014-03-07 22:19:53 -0300

    libreria basica de entrada y salida agregada

---
+++ bot.php
@@ -0,0 +1,18 @@
+<?php if(!defined('STDIN')) die('ONLY EXECUTE COMMAND-LINE-INTERFACE');
+
+/** ThiefBot PHP 5
+ *  @designer: Alexander171294
+ *  @Proyect Name: ThiefBot
+ *  @contact: Alexander171294@gmail.com
+ *  @Status: Prototype
+ *  @Date: 08/2/14  
+ *  @info: proyecto desarrollado por Alexander171294
+ */           
+#ini_set('display_errors',false);
+ 
+ // definimos saltos de linea
+ define('NL', "\r\n");
+ define('DS', "/"); // directory separator
+ 
+ // requerimos la libreria basica
+ require(dirname(__FILE__).DS.'libs'.DS.'stdio.php');
---
+++ libs/stdio.php
@@ -0,0 +1,57 @@
+<?php
+
+<?php
+
+/** IBOT SOCKET PHP5
+ *  @designer: Alexander171294
+*  @Proyect Name: PHPIBot 2
+*  @contact: Alexander171294@gmail.com
+*  @Status: Prototype
+*  @Date: 02/12/13
+*  @info: proyecto desarrollado por el equipo UnderC0de++
+*/
+
+// FUNCIONES EXTENSIBLES
+/*
+ * Si es necesario cambiar la salida, solo modifcamos esta función.
+*/
+
+
+// si no existe definimos directory separator
+if(!defined('DS')) define('DS', "\\");
+
+// standard out
+function std_out($string)
+{
+	echo utf8_decode(utf8_encode($string)).NL;
+}
+
+// standard input
+function std_in($format)
+{
+	$res = null;
+	$line = trim(fgets(STDIN)); // reads one line from STDIN
+	fscanf(STDIN, "$format\n", $res); // reads number from STDIN
+	return $res;
+}
+// standard includes
+function std_inc($file)
+{
+	std_out('[*] LOAD module='.$file);
+	require(dirname(__FILE__).DS.'..'.DS.$file);
+}
+// standard file_exists
+function std_exists($file)
+{
+	return file_exists(dirname(__FILE__).DS.'..'.DS.$file);
+}
+// standard input
+function std_open($file)
+{
+	return file_get_contents(dirname(__FILE__).DS.'..'.DS.$file);
+}
+
+function std_put($file, $content)
+{
+	file_put_contents(dirname(__FILE__).DS.'..'.DS.$file, $content);
+}
